1a4371d2b3b35b9613f62da9187bf7af110ec821
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-11-11 20:58:06 +0200

    support markdown in dynamic_content macro

--- JumpscaleLibs/tools/markdowndocs/macros/templates/dynamic_content_script.js
+++ JumpscaleLibs/tools/markdowndocs/macros/templates/dynamic_content_script.js
@@ -1,1 +1,1 @@
-callActorAndRender('{{ actor }}', '{{ method }}', {{ args | safe }}, '{{ container_id }}');
+callActorAndRender('{{ actor }}', '{{ method }}', {{ args | safe }}, '{{ container_id }}', {% if markdown %}true{% else %} false{% endif %});
