0dbabae7e292e2b421802673e2e0448d763f35a1
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-01-26 14:30:36 +0200

    remove incomplte tool

--- JumpscaleLibs/tools/bcdbsync/bcdbsyncfactory.py
+++
@@ -1,9 +0,0 @@
-from Jumpscale import j
-
-from .bcdbsync import BCDBSync
-
-
-class BCDBSyncFactory(j.baseclasses.object_config_collection_testtools):
-    __jslocation__ = "j.tools.bcdbsync"
-
-    _CHILDCLASS = BCDBSync
