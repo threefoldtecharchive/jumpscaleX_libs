292cb22e25ebdd3cb8e464f78196cf1bfb83c81b
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-03-06 16:17:14 +0100

    add zosv2 SAL

---
+++ JumpscaleLibs/sal/zosv2/container.py
@@ -0,0 +1,107 @@
+import netaddr
+from Jumpscale import j
+import base58
+from nacl import signing
+from .id import _next_workload_id
+from nacl import public
+import binascii
+
+
+class ContainerGenerator:
+    def create(
+        self,
+        reservation,
+        node_id,
+        network_name,
+        ip_address,
+        flist,
+        env={},
+        cpu=1,
+        memory=1024,
+        entrypoint="",
+        interactive=False,
+        secret_env={},
+        public_ipv6=False,
+        storage_url="zdb://hub.grid.tf:9900",
+    ):
+        """
+        add a container to the reservation
+        
+        :param reservation: [description]
+        :type reservation: [type]
+        :param node_id: [description]
+        :type node_id: [type]
+        :param network_name: [description]
+        :type network_name: [type]
+        :param ip_address: [description]
+        :type ip_address: [type]
+        :param flist: [description]
+        :type flist: [type]
+        :param env: [description], defaults to {}
+        :type env: dict, optional
+        :param cpu: [description], defaults to 1
+        :type cpu: int, optional
+        :param memory: [description], defaults to 1024
+        :type memory: int, optional
+        :param entrypoint: [description], defaults to ""
+        :type entrypoint: str, optional
+        :param interactive: [description], defaults to False
+        :type interactive: bool, optional
+        :param storage_url: [description], defaults to "zdb://hub.grid.tf:9900"
+        :type storage_url: str, optional
+        :raises j.excpetions.Input: [description]
+        :return: [description]
+        :rtype: [type]
+        """
+
+        cont = reservation.data_reservation.containers.new()
+        cont.node_id = node_id
+        cont.workload_id = _next_workload_id(reservation)
+
+        cont.flist = flist
+        cont.storage_url = storage_url
+        cont.environment = env
+        cont.secret_environment = secret_env
+        cont.entrypoint = entrypoint
+        cont.interactive = interactive
+
+        nw = None
+        for nw in reservation.data_reservation.networks:
+            if nw.name == network_name:
+                ip = netaddr.IPAddress(ip_address)
+                subnet = netaddr.IPNetwork(nw.iprange)
+                if ip not in subnet:
+                    raise j.excpetions.Input(
+                        f"ip address {str(ip)} is not in the range of the network resource of the node {str(subnet)}"
+                    )
+
+        net = cont.network_connection.new()
+        net.network_id = network_name
+        net.ipaddress = ip_address
+
+        cont.capacity.cpu = cpu
+        cont.capacity.memory = memory
+
+        return cont
+
+    def corex_connect(self, ip, port=7681):
+        """
+        return a coreX client
+        
+        :param ip: ip address of the container
+        :type ip: str
+        :param port: listening port of corex process, defaults to 7681
+        :type port: int, optional
+        :return: j.clients.corex
+        """
+        return j.clients.corex.new(name=j.data.idgenerator.generateGUID(), addr=ip, port=port, autosave=False)
+
+    def encrypt_secret(self, node_id, value):
+        key = base58.b58decode(node_id)
+        pk = signing.VerifyKey(key)
+        encryption_key = pk.to_curve25519_public_key()
+
+        box = public.SealedBox(encryption_key)
+        result = box.encrypt(value.encode())
+
+        return binascii.hexlify(result)
---
+++ JumpscaleLibs/sal/zosv2/crypto.py
@@ -0,0 +1,31 @@
+from nacl import public
+from nacl.encoding import Base64Encoder
+from nacl.public import SealedBox
+from nacl.signing import SigningKey, VerifyKey
+
+from Jumpscale import j
+
+
+def encrypt_for_node(public_key, payload):
+    """
+    encrypt payload with the public key of a node
+    so only the node itself can decrypt it
+    
+    use this if you have sensitive data to send in a reservation
+    
+    :param public_key: public key of the node, hex-encoded.
+    :type public_key: str
+    :param payload: any data you want to encrypt
+    :type payload: bytes
+    :return: encrypted data. you can use this safely into your reservation data
+    :rtype: str
+    """
+    node_public_bin = j.data.hash.hex2bin(public_key)
+    node_public = VerifyKey(node_public_bin)
+    box = SealedBox(node_public.to_curve25519_public_key())
+
+    if isinstance(payload, str):
+        payload = payload.encode()
+
+    encrypted = box.encrypt(payload)
+    return j.data.hash.bin2hex(encrypted)
