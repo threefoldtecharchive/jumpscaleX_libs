69d542201b49a95be7c714b4751a0b1beb77052e
Rana Tarek Hassan <tarekr@codescalers.com>
2020-03-10 12:30:07 +0200

    Create multiple nodes in network in chatflow sal instead of single node

--- JumpscaleLibs/sal/chatflow/Chatflow.py
+++ JumpscaleLibs/sal/chatflow/Chatflow.py
@@ -20,18 +20,32 @@ class Chatflow(j.baseclasses.object):
             ips.append(ip.format())
         return ips
 
-    def network_configure(self, bot, reservation, node):
+    def nodes_get(self, number_of_nodes, ip_version):
+        nodes = j.sal.zosv2.nodes_finder.nodes_search()
+        nodes_selected = list()
+        if ip_version == "IPV4":
+            for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip4, nodes):
+                if len(nodes_selected) < number_of_nodes:
+                    nodes_selected.append(node)
+        else:
+            for node in filter(j.sal.zosv2.nodes_finder.filter_public_ip6, nodes):
+                if len(nodes_selected) < number_of_nodes:
+                    nodes_selected.append(node)
+
+        return nodes_selected
+
+    def network_configure(self, bot, reservation, nodes):
         """
         bot: Gedis chatbot object from chatflow
         reservation: reservation object from schema
-        node: node object from explorer
+        node: list of node objects from explorer
 
         return reservation (Object) , config of network (dict)
         """
-        ip_range = self.get_ip_range(bot)
-        return self.get_network(bot, reservation, ip_range, node)
+        ip_range = self.ip_range_get(bot)
+        return self.network_get(bot, reservation, ip_range, nodes)
 
-    def get_ip_range(self, bot):
+    def ip_range_get(self, bot):
         """
         bot: Gedis chatbot object from chatflow
         return ip_range from user or generated one
@@ -51,12 +65,12 @@ class Chatflow(j.baseclasses.object):
             ip_range = str(first_digit) + "." + str(second_digit) + ".0.0/16"
         return ip_range
 
-    def get_network(self, bot, reservation, ip_range, node):
+    def network_get(self, bot, reservation, ip_range, nodes):
         """
         bot: Gedis chatbot object from chatflow
         reservation: reservation object from schema
         ip_range: ip range for network eg: "10.70.0.0/16"
-        node: node object from explorer
+        node: list of node objects from explorer
 
         return reservation (Object) , config of network (dict)
         """
@@ -65,10 +79,10 @@ class Chatflow(j.baseclasses.object):
         user_choice = bot.single_choice("Create a new network or use an existing one.", network_choice)
         if user_choice == "New network":
             network_name = bot.string_ask(
-                "Please add a network name. Don't forget to save it. otherwise Leave empty for using a generated name"
+                "Please add a network name. Don't forget to save it. Otherwise leave it empty for using a generated name"
             )
             network = j.sal.zosv2.network.create(reservation, ip_range, network_name)
-            network_config = self._register_network(bot, reservation, ip_range, network, node)
+            network_config = self._register_network(bot, reservation, ip_range, network, nodes)
         else:
             res = "# This feature is not available yet."
             res = j.tools.jinja2.template_render(text=res)
@@ -76,13 +90,13 @@ class Chatflow(j.baseclasses.object):
 
         return reservation, network_config
 
-    def _register_network(self, bot, reservation, ip_range, network, node):
+    def _register_network(self, bot, reservation, ip_range, network, nodes):
         """
         bot: Gedis chatbot object from chatflow
         reservation: reservation object from schema
         ip_range: ip range for network eg: "10.70.0.0/16"
         network: network object from schema
-        node: node object from explorer
+        node: list of node objects from explorer
 
         return config of network (dict)
         """
@@ -91,23 +105,36 @@ class Chatflow(j.baseclasses.object):
         network_range += 256
         network_node = str(network_range) + "/24"
 
-        avaliable_ips = self.get_all_ips(network_node)
-        string_ips = []
-        for ip in avaliable_ips:
-            string_ips.append(ip.format())
+        ip_addresses = list()
+        wg_configs = list()
 
-        ip_address = bot.drop_down_choice("Please choose ip address of the container", string_ips)
-        network_config["ip_address"] = ip_address
-        j.sal.zosv2.network.add_node(network, node.node_id, network_node)
-        network_range += 256
-        network_node = str(network_range) + "/24"
+        for i, node_selected in enumerate(nodes):
+            network_range += 256
+            network_node = str(network_range) + "/24"
 
-        wg_quick = j.sal.zosv2.network.add_access(network, node.node_id, network_node, ipv4=True)
-        network_config["wg"] = wg_quick
-        expiration = j.data.time.epoch + (3600 * 24 * 365)
+            avaliable_ips = self.get_all_ips(network_node)
+            string_ips = []
+            for ip in avaliable_ips:
+                string_ips.append(ip.format())
+
+            # user chooses the ip to be used for the node
+            ip_address = bot.drop_down_choice(f"Please choose the ip address of node {i+1}", string_ips)
+            ip_addresses.append(ip_address)  # ip in position i, corresponds to the node in position i in nodes_selected
+            string_ips.remove(ip_address)
+
+            j.sal.zosv2.network.add_node(network, node_selected.node_id, network_node)
+            network_range += 256
+            network_node = str(network_range) + "/24"
+
+            wg_quick = j.sal.zosv2.network.add_access(network, node_selected.node_id, network_node, ipv4=True)
+            wg_configs.append(wg_quick)
+
         network_config["name"] = network.name
+        network_config["ip_addresses"] = ip_addresses
+        network_config["wg"] = wg_configs
 
         # register the reservation
+        expiration = j.data.time.epoch + (3600 * 24 * 365)
         j.sal.zosv2.reservation_register(reservation, expiration)
 
         return network_config
