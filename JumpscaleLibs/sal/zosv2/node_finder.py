359b88b7200fe2dd064c751a070bc2c82ba55862
ahelsayd <ahmed.m.elsayed93@gmail.com>
2020-03-17 16:47:41 +0200

    add node search by capacity
    add node search by farm name

--- JumpscaleLibs/sal/zosv2/node_finder.py
+++ JumpscaleLibs/sal/zosv2/node_finder.py
@@ -21,13 +21,44 @@ class NodeFinder:
     def filter_public_ip6(self, node):
         return filter_public_ip(node, 6)
 
-    def nodes_search(self, farm_id=None, country=None, city=None, cru=None, sru=None, mru=None, hru=None):
+    def nodes_by_capacity(self, cru=None, sru=None, mru=None, hru=None):
+        nodes = self._actor_directory.nodes.list(cru=cru, sru=sru, mru=mru, hru=hru).nodes
+        for node in nodes:
+            total = node.total_resources
+            reserved = node.reserved_resources
+            if cru and total.cru - max(0, reserved.cru) < cru:
+                continue
+
+            if mru and total.mru - max(0, reserved.mru) < mru:
+                continue
+
+            if sru and total.sru - max(0, reserved.sru) < sru:
+                continue
+
+            if hru and total.hru - max(0, reserved.hru) < hru:
+                continue
+
+            yield node
+
+    def nodes_search(
+        self, farm_id=None, farm_name=None, country=None, city=None, cru=None, sru=None, mru=None, hru=None
+    ):
         """
         list return all the nodes
         
         :return: list of nodes
         :rtype: list
         """
+        if not farm_id and not farm_name:
+            farm_name = "freefarm"
+
+        if farm_name:
+            try:
+                farm = self._actor_directory.farms.get(name=farm_name)
+                farm_id = farm.id
+            except j.exceptions.NotFound:
+                return []
+
         return self._actor_directory.nodes.list(
             farm_id=farm_id, country=country, city=city, cru=cru, sru=sru, mru=mru, hru=hru
         ).nodes
