42ee7d17e27281659a5ef12c0fb44a368c69e579
alichaddad <chaddada@greenitglobe.com>
2020-03-19 12:49:48 +0200

    fix corex test (#124)

--- JumpscaleLibs/clients/corex/CoreXFactory.py
+++ JumpscaleLibs/clients/corex/CoreXFactory.py
@@ -12,7 +12,6 @@ class CoreXClientFactory(j.baseclasses.object_config_collection_testtools):
     def _init(self, **kwargs):
         pass
 
-    @skip("https://github.com/threefoldtech/jumpscaleX_core/issues/524")
     def test(self):
         """
         kosmos 'j.clients.corex.test()'
@@ -20,67 +19,54 @@ class CoreXClientFactory(j.baseclasses.object_config_collection_testtools):
         """
 
         def test(passw=False):
+            try:
+                j.clients.corex.reset()
 
-            j.clients.corex.reset()
+                s = j.servers.corex.default
+                s.port = 8002
 
-            s = j.servers.corex.default
-            s.port = 8002
+                if passw:
+                    s.user = "user"
+                    s.passwd = "pass"
+                    cl = self.get(name="test", addr="localhost", port=s.port, login="user", passwd_="pass")
+                else:
+                    cl = self.get(name="test", addr="localhost", port=s.port)
 
-            if passw:
-                s.user = "user"
-                s.passwd = "pass"
-                cl = self.get(name="test", addr="localhost", port=s.port, login="user", passwd_="pass")
-            else:
-                cl = self.get(name="test", addr="localhost", port=s.port)
+                s.start()
 
-            s.start()
+                assert cl.process_list() == []
 
-            cl2 = s.client
+                r = cl.process_start("mc")
 
-            assert cl.process_list() == []
+                r2 = cl.process_info_get(r["id"])
+                assert r["id"] == r2["id"]
 
-            # j.shell()
+                r2 = cl.process_info_get(corex_id=r["id"])
+                assert r["id"] == r2["id"]
 
-            r = cl.process_start("mc", "mc")
+                r2 = cl.process_info_get(corex_id=r["id"])
+                assert r["id"] == r2["id"]
 
-            # lets now see we can get the right objects back
+                r2 = cl.process_info_get(pid=r["pid"])
 
-            r2 = cl.process_get("mc")
-            assert r.data.id == r2.data.id
-            assert r.corex_id == r2.corex_id
+                assert r["id"] == r2["id"]
 
-            r2 = cl.process_get(id=r.data.id)
-            assert r.data.id == r2.data.id
-            assert r.corex_id == r2.corex_id
+                cl.ui_link_print(r["id"])
 
-            r2 = cl.process_get(corex_id=r.corex_id)
-            assert r.data.id == r2.data.id
-            assert r.corex_id == r2.corex_id
+                # lets do the stop test
+                cl.process_kill(r["id"])
+                r = cl.process_info_get(r["id"])
+                assert r["state"] == "stopped"
 
-            r2 = cl.process_get(pid=r.pid)
+                r3 = cl.process_start("ls /")
+                print(cl.process_log_get(r3["id"]))
+                assert r3["status"] == "success"
+                r3 = cl.process_info_get(r3["id"])
+                assert r3["state"] == "stopped"
 
-            assert r.data.id == r2.data.id
-            assert r.corex_id == r2.corex_id
-
-            # now we collected processes in multiple ways
-
-            r.ui()
-
-            # lets do the stop test
-            r.kill()
-            r.refresh()
-            assert r.state == "stopped"
-
-            r3 = cl.process_start("ls", "ls /")
-            print(r3.logs)
-            assert r3.state == "ok"
-            r3.refresh()
-            assert r3.state == "stopped"
-
-            cmd0.stop()
-            j.data.corex_process.reset()
-            j.clients.corex.reset()
-            # j.shell()
+                j.clients.corex.reset()
+            finally:
+                cl.process_clean()
 
         print("TEST NO AUTH")
         test(False)
