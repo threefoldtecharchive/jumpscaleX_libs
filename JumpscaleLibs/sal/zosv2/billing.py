5e82a5bb4dbd03c43e3ed4e1da5b008dde2cfa73
Jo De Boeck <deboeck.jo@gmail.com>
2020-06-28 14:03:56 +0200

    Check wallet before attempting transfer for trustline and funds
    See: #237
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- JumpscaleLibs/sal/zosv2/billing.py
+++ JumpscaleLibs/sal/zosv2/billing.py
@@ -8,6 +8,14 @@ TFT_ISSUER_TEST = "GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3"
 ASSET_CODE = "TFT"
 
 
+class InsufficientFunds(Exception):
+    pass
+
+
+class MissingTrustLine(Exception):
+    pass
+
+
 class Billing:
     def payout_farmers(self, client, reservation_response):
         """
@@ -21,13 +29,24 @@ class Billing:
 
         transaction_hashes = []
         reservation_id = reservation_response.reservation_id
-        escrow_informations = reservation_response.escrow_information
         asset = reservation_response.escrow_information.asset
         total_amount = sum([d.total_amount for d in reservation_response.escrow_information.details])
         total_amount = Decimal(total_amount) / Decimal(1e7)
         total_amount = '{0:f}'.format(total_amount)
 
         escrow_address = reservation_response.escrow_information.address
+
+        total_amount_dec = Decimal(total_amount) / Decimal(1e7)
+        total_amount = "{0:f}".format(total_amount_dec)
+
+        escrow_address = reservation_response.escrow_information.address
+        for balance in client.get_balances().balances:
+            if f"{balance.asset_code}:{balance.asset_issuer}" == asset:
+                if total_amount_dec > Decimal(balance.balance):
+                    raise InsufficientFunds(f"Wallet {client.name} does not have enough funds to pay for {total_amount_dec:0f} {asset}")
+                break
+        else:
+            raise MissingTrustLine(f"Wallet {client.name} does not have a valid trustline to pay for {asset}")
         try:
             txhash = client.transfer(escrow_address, total_amount, asset=asset, memo_text=str(reservation_id))
             transaction_hashes.append(txhash)
